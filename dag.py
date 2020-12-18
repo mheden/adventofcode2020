900b5be11107727d861da26cb67cc0281d8ded91
Mikael Heden <mikael@heden.net>
2020-12-18 17:03:01 +0100

    dag: add traversal functions

--- dag.py
+++ dag.py
@@ -1,9 +1,12 @@
+from collections import deque
+
+
 class DAG:
     def __init__(self):
         self.nodes = dict()
         self.edges = dict()
 
-    def add_edge(self, from_, to_, weight):
+    def add_edge(self, from_, to_, weight=1):
         for node in [from_, to_]:
             if node not in self.nodes:
                 self.nodes[node] = {"pre": set(), "suc": set()}
@@ -41,3 +44,73 @@ class DAG:
             updated = get_ancestors(result)
             result |= updated
         return result
+
+    def descendants_pre(self, root):
+        def _path(root):
+            res = [root]
+            for successor in sorted(self.successors(root)):
+                res += _path(successor)
+            return res
+        return _path(root)
+
+
+    def descendants_post(self, root):
+        def _path(root):
+            res = []
+            for successor in sorted(self.successors(root)):
+                res += _path(successor)
+            return res + [root]
+        return _path(root)
+
+    def pathsfrom(self, from_):
+        def _pathsfrom(from_, path):
+            path.append(from_)
+
+            if len(self.successors(from_)) == 0:
+                paths.append(tuple(path))
+
+            for successor in sorted(self.successors(from_)):
+                _pathsfrom(successor, path)
+            path.pop()
+
+        paths = []
+        path = deque()
+        _pathsfrom(from_, path)
+        return paths
+
+    def pathsfromto(self, from_, to_):
+        def _pathsfrom(from_, to_, path):
+            path.append(from_)
+
+            if len(self.successors(from_)) == 0:
+                if from_ == to_:
+                    paths.append(tuple(path))
+
+            for successor in sorted(self.successors(from_)):
+                _pathsfrom(successor, to_, path)
+            path.pop()
+
+        paths = []
+        path = deque()
+        _pathsfrom(from_, to_, path)
+        return paths
+
+
+if __name__ == "__main__":
+    dag = DAG()
+    dag.add_edge('a', 'aa')
+    dag.add_edge('a', 'ab')
+    dag.add_edge('aa', 'aaa')
+    dag.add_edge('ab', 'aaab')
+    dag.add_edge('aaa', 'aaaa')
+    dag.add_edge('aaa', 'aaab')
+    dag.add_edge('ab', 'aba')
+    dag.dump()
+
+    # print(dag.descendants_pre('a'))
+    # print(dag.descendants_post('a'))
+
+    # print(dag.pathsfrom('a'))
+    # print(dag.pathsfrom('aaa'))
+
+    print(dag.pathsfromto('a', 'aaab'))
